27967de3e674d1f3f332f2bfecd381fc16e2c9ab
Célian HAMON <hamoncelian@gmail.com>
2026-02-26 21:55:08 +0100

    fix: use correct RSA public key from auth-service

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,20 @@ AppDataSource.initialize()
     })
     .catch((e) => console.log(e));
 
-// Hardcoded JWT_PUBLIC_KEY_BASE64 as requested to fix SPKI formatting issue
+// Hardcoded JWT_PUBLIC_KEY_BASE64 to fix SPKI formatting issue
 process.env.JWT_PUBLIC_KEY_BASE64 = `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEwHwFKvvE0vjcJ8sbWzuupIIwIRmw
-04OvPHNrTvGmM0Zr6Q45boKo4InZMVwLrzXfI9e3SQRFQ7LToT0OLKrf+Q==
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAn6v2wD5P6ycIz00CH8oU
+FzvJIW/ob9f4On3aJr42yrQYUS56LfuN1P4BgApWP1d2u6AgLBA0yq8X6j1+rJgd
+R6YvGky28hP9+74F66s8EXXxrEfI+Y9awD7bY8vKmAIkam7f6Otad5nlJKPzLN9s
+eSuVfP/rl+6MJxr7LykZ2rA+ZOeNi9BHoJ2QNvV/jzBDrxpVuSmcLA0qvcDtIqy0
+40xYP7sw4yeaZ6Pc062DaurL4cfuNNcAkuDEvYilWBsYH15jJl5S/jbZqggOHAgV
+M3aCWU7T/NMLPTxRdnt3w68M2aQOc+DqELOuIh6jkxD0mNN/+xfyQyVJBCdcSgxo
+0RkbKZOJMk+Kl7lzRV+NLyJtqdx/wdNLAA65Gx7USjlNHb7HeMsLWrY8ZVBiZRLX
++ReuO8+aZI3CvmUzAYpnMbg+GJ1SZt8Zzx3KSm/0ay3Bt5tYwvr5okToJvR6Uh9r
+rwDuVerri4sNxrJ6Bk59kZZrk3kW5AuNbEIvZUyDhPAo2p/HqU44TmcftQSvI0k2
+sKGUELix5ClhB89q7Ypch4H4SUu6CiNs3V4nPPJ2dnRXnrYkoQocFZsTdBLjUbSg
+G/+1qlk1Bl64I0ZqZkgBImSrJF48e2Y+qgpQy9cHcujA1uhK9V0CfwT45eQBK9kV
+4a1VWyZO3NWV08oELwZaly8CAwEAAQ==
 -----END PUBLIC KEY-----`;
 
 import { authMiddleware } from "@good-food/utils";
